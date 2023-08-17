fed4fbc1100227b99bf356c8d944235eb52e9bc5
winnsterx <winnie.xiaoy@gmail.com>
2023-08-16 17:09:24 -0700

    has functional, static orderflow sankey

--- visual_analysis.py
+++ visual_analysis.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 import analysis
 
@@ -31,32 +32,71 @@ def overlap_searcher_frequency_maps(bottom, top):
     plt.show(block=False)
 
 
-def searcher_builder_order_flow(map, agg, title):
-    # Process data
-    nodes = list(agg.keys()) + list(map.keys())
+def create_a_flow(map, agg, searcher=None):
+    if searcher is None:
+        nodes = list(agg.keys()) + list(map.keys())
+    else: 
+        nodes = list(searcher) + list(map.keys())
+
     source_indices = []
     target_indices = []
     values = []
 
     for builder, searchers in map.items():
-        for searcher, tx_count in searchers.items():
-            source_indices.append(nodes.index(searcher))
-            target_indices.append(nodes.index(builder))
-            values.append(tx_count)
+        for s, tx_count in searchers.items():
+            if searcher is None or s == searcher:
+                if s not in nodes: 
+                    continue
+                source_indices.append(nodes.index(s))
+                target_indices.append(nodes.index(builder))
+                values.append(tx_count)
 
-    hover_texts = [f"Outgoing flow: {agg[searcher]}" for searcher in agg]
-    hover_texts.extend([''] * len(map.keys()))
-
-    # Create Sankey diagram
-    fig = go.Figure(
-        go.Sankey(
-            node=dict(pad=15, thickness=20, line=dict(color="black", width=0.5), label=nodes),
-            link=dict(source=source_indices, target=target_indices, value=values),
-        )
+    return go.Sankey(
+        node=dict(pad=15, thickness=20, line=dict(color="black", width=0.5), label=nodes),
+        link=dict(source=source_indices, target=target_indices, value=values)
     )
 
-    # Layout
+
+
+def searcher_builder_orderflow(map, agg, title):
+    # Create a figure with a dropdown menu
+    fig = make_subplots(specs=[[{'type': 'sankey'}]])
+    fig.add_trace(create_a_flow(map, agg))
+
+    # Create a dropdown menu with all the searchers and an option for all searchers
+    # menu_buttons = [dict(args=[{"data": [create_a_flow(map, agg)]}], label='All searchers', method='update')]
+    # for searcher in agg:
+    #     menu_buttons.append(
+    #         dict(
+    #             args=[{"data": [create_a_flow(map, agg, searcher)]}],
+    #             label=searcher,
+    #             method='update'
+    #         )
+    #     )
+
+    # # Update the layout to add the dropdown menu
+    # fig.update_layout(
+    #     updatemenus=[
+    #         dict(
+    #             buttons=menu_buttons,
+    #             direction="down",
+    #             pad={"r": 10, "t": 10},
+    #             showactive=True,
+    #             x=0.1,
+    #             xanchor="left",
+    #             y=1.15,
+    #             yanchor="top"
+    #         ),
+    #     ],
+    #     annotations=[
+    #         dict(text="Select searcher:", showarrow=False, x=0, y=1.085, yref="paper", align="left")
+    #     ]
+    # )
+
+    # Add layout options
     fig.update_layout(title_text=title, font_size=10)
+
+    # Show the figure
     fig.show()
 
 
@@ -64,6 +104,6 @@ if __name__ == "__main__":
     map = analysis.load_dict_from_json("atomic/builder_atomic_map.json")
     agg = analysis.load_dict_from_json("atomic/atomic_searchers_agg.json")
 
-    top_map, top_agg = analysis.get_map_in_range(map, agg, 0.9)
+    map, agg = analysis.get_map_in_range(map, agg, 0.9)
 
-    searcher_builder_order_flow(top_map, top_agg, "searcher-builder orderflow")
+    searcher_builder_orderflow(map, agg, "searcher-builder orderflow")
